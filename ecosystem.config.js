e476ec7071e7920518948dcb91947d47c0a83afa
Dominik Voss <d.voss@porta.de>
2021-10-28 15:43:28 +0200

    rem post setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,15 +11,7 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:domvo/cratest.git",
       path: "/home/dome/cratest",
-      // Pre-setup command or path to a script on your local machine
-      // 'pre-setup': "source ~/.nvm/nvm.sh",
-      // Post-setup commands or path to a script on the host machine
-      // eg: placing configurations in the shared dir etc
-      // 'post-setup': "npm install && npm run build && pm2 startOrRestart ecosystem.config.json",
-      'post-setup': "pwd",
-      // pre-deploy action
-      // 'pre-deploy-local': "echo 'This is a local executed command'",
-      // post-deploy action
+      // 'post-setup': "pwd",
       'post-deploy': "touch ./hello.txt",
     },
   }
