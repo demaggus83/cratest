ab83bb85f7489ab620d4bc0ad25b7a6bdc6f270f
Dominik Voss <d.voss@porta.de>
2021-10-28 15:39:51 +0200

    pwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,8 @@ module.exports = {
       // 'pre-setup': "source ~/.nvm/nvm.sh",
       // Post-setup commands or path to a script on the host machine
       // eg: placing configurations in the shared dir etc
-      'post-setup': "npm install && npm run build && pm2 startOrRestart ecosystem.config.json",
+      // 'post-setup': "npm install && npm run build && pm2 startOrRestart ecosystem.config.json",
+      'post-setup': "pwd",
       // pre-deploy action
       // 'pre-deploy-local': "echo 'This is a local executed command'",
       // post-deploy action
