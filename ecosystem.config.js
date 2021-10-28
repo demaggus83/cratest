983e37f48a6b8424baf691e04c7a74f6045436c0
Dominik Voss <d.voss@porta.de>
2021-10-28 14:48:43 +0200

    add ecosystem cfg

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: "serve -s build"
+  }],
+  deploy: {
+    approval: {
+      user: "dome",
+      host: ["165.227.157.221"],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      repo: "git@github.com:domvo/cratest.git",
+      path: "/home/dome/cratest",
+      // Pre-setup command or path to a script on your local machine
+      // 'pre-setup': "source ~/.nvm/nvm.sh",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      'post-setup': "npm install && npm run build",
+      // pre-deploy action
+      // 'pre-deploy-local': "echo 'This is a local executed command'",
+      // post-deploy action
+      // 'post-deploy': "npm install; npm i -g serve;",
+    },
+  }
+}
