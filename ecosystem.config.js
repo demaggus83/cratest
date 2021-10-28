7162f6cef52c6d4de3e4b4a955b25d8cc825bd9f
Dominik Voss <d.voss@porta.de>
2021-10-28 14:54:02 +0200

    add post deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       // pre-deploy action
       // 'pre-deploy-local': "echo 'This is a local executed command'",
       // post-deploy action
-      // 'post-deploy': "npm install; npm i -g serve;",
+      'post-deploy': "pm2 startOrRestart ecosystem.config.json",
     },
   }
 }
